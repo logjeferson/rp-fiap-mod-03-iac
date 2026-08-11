1600cc7e09cd5471359297ae03f2d7677076056d
Jeferson Luna <logjeferson@gmail.com>
2026-08-10 22:25:36 -0300

    feature: IaC v1.5.1

--- terraform-modules/argocd.tf
+++ terraform-modules/argocd.tf
@@ -6,26 +6,28 @@ resource "helm_release" "argocd" {
   create_namespace = true
   version          = "7.4.4"
 
-  set = [
-    {
-      name  = "server.ingress.enabled"
-      value = "true"
-    },
-    {
-      name  = "server.ingress.ingressClassName"
-      value = "nginx"
-    },
-    {
-      name  = "server.ingress.hosts[0]"
-      value = "argocd.mysandbox.com.br"
-    },
-    {
-      name  = "server.ingress.https"
-      value = "true"
-    },
-    {
-      name  = "server.extraArgs[0]"
-      value = "--insecure"
-    }
-  ]
+  set {
+    name  = "server.ingress.enabled"
+    value = "true"
+  }
+
+  set {
+    name  = "server.ingress.ingressClassName"
+    value = "nginx"
+  }
+
+  set {
+    name  = "server.ingress.hosts[0]"
+    value = "argocd.meudominio.com"
+  }
+
+  set {
+    name  = "server.ingress.https"
+    value = "true"
+  }
+
+  set {
+    name  = "server.extraArgs[0]"
+    value = "--insecure"
+  }
 }
