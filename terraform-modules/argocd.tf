20504c41da3abdfcc61054172f53ec8b0a1dbe11
Jeferson Luna <logjeferson@gmail.com>
2026-08-11 00:27:05 -0300

    feature: IaC v1.6.0

--- terraform-modules/argocd.tf
+++ terraform-modules/argocd.tf
@@ -2,7 +2,7 @@ resource "helm_release" "argocd" {
   name             = "argocd"
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
-  namespace        = "argocd"
+  namespace        = "eks-argocd-namespace"
   create_namespace = true
   version          = "7.4.4"
 
