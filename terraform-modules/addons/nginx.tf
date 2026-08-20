19ec9bb638c24c12a74e34d6e514c617836e0b06
Jeferson Luna <logjeferson@gmail.com>
2026-08-20 15:59:12 -0300

    feature: IaC v1.9.0

--- terraform-modules/addons/nginx.tf
+++ terraform-modules/addons/nginx.tf
@@ -2,7 +2,7 @@ resource "helm_release" "ingress_nginx" {
   name             = "ingress-nginx"
   repository       = "https://kubernetes.github.io/ingress-nginx"
   chart            = "ingress-nginx"
-  namespace        = "ingress-nginx"
+  namespace        = "eks-namespace"
   create_namespace = true
   version          = "4.10.1"
   values = [
